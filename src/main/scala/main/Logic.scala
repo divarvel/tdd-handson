f11c6b1bc5ea83053421ae1ffc0127e18b35e779
Clement Delafargue <clement@delafargue.name>
2015-12-08 18:21:25 +0100

    Add DTO

--- src/main/scala/main/Logic.scala
+++ src/main/scala/main/Logic.scala
@@ -4,11 +4,11 @@ import scala.util.Try
 import play.api.libs.json._
 
 object logic {
+
   case class Order(prices: Seq[Float], quantities: Seq[Int], country: String, reduction: String)
   implicit val orderFormat = Json.format[Order]
   case class Answer(total: Float)
   implicit val answerFormat = Json.format[Answer]
-
 
   def processRequest(body: String): String = ???
 }
