f92f8e7d002bc4ea8077b7369277299283189716
Clement Delafargue <clement@delafargue.name>
2015-12-08 14:27:01 +0100

    Implement country taxes

--- src/main/scala/main/Logic.scala
+++ src/main/scala/main/Logic.scala
@@ -28,7 +28,55 @@ object logic {
     } yield Answer(withDiscount)
   }
 
-  def getTotalPrice(order: Order): Option[Float] = ???
-  def applyTaxes(totalPrice: Float, country: String): Option[Float] = ???
-  def applyDiscount(withTaxes: Float, reduction: String): Option[Float] = ???
+  def getTotalPrice(order: Order): Option[Float] = {
+    val total = order.prices.zip(order.quantities).map({case (p,q) => p * q}).sum
+
+    Some(total).filter(_ => order.prices.length == order.quantities.length)
+  }
+
+  def applyTaxes(totalPrice: Float, country: String): Option[Float] = {
+    ratesByCountry.get(country) map { rate =>
+      totalPrice * (1 + rate)
+    }
+  }
+  def applyDiscount(withTaxes: Float, reduction: String): Option[Float] = {
+    getDiscountRate(reduction) map { rate =>
+      withTaxes * (1 - rate)
+    }
+  }
+
+
+
+  def getDiscountRate(discount: String): Option[Float] = ???
+
+  val ratesByCountry = Map(
+    "DE" -> 0.20f,
+    "UK" -> 0.21f,
+    "FR" -> 0.20f,
+    "IT" -> 0.25f,
+    "ES" -> 0.19f,
+    "PL" -> 0.21f,
+    "RO" -> 0.20f,
+    "NL" -> 0.20f,
+    "BE" -> 0.24f,
+    "EL" -> 0.20f,
+    "CZ" -> 0.19f,
+    "PT" -> 0.23f,
+    "HU" -> 0.27f,
+    "SE" -> 0.23f,
+    "AT" -> 0.22f,
+    "BG" -> 0.21f,
+    "DK" -> 0.21f,
+    "FI" -> 0.17f,
+    "SK" -> 0.18f,
+    "IE" -> 0.21f,
+    "HR" -> 0.23f,
+    "LT" -> 0.23f,
+    "SI" -> 0.24f,
+    "LV" -> 0.20f,
+    "EE" -> 0.22f,
+    "CY" -> 0.21f,
+    "LU" -> 0.25f,
+    "MT" -> 0.20f
+  )
 }
